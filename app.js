a05dd0368d6b808dab142ed9d9e3a9649a436142
Irtaza Ahmed <irtazaahmedk@gmail.com>
2025-01-31 21:15:23 +0500

    improve css and responsive

--- app.js
+++ app.js
@@ -7,62 +7,62 @@ let img = document.getElementById("circle-img")
 
 function themebtn() {
 
-circle.classList.toggle("circle-anime") 
+    circle.classList.toggle("circle-anime")
 
-if (img.src.includes("/assets/sun1.png")) {
-    img.src = "/assets/moon2.png"
-} else {
-    img.src = "/assets/sun1.png"
+    if (img.src.includes("/assets/sun1.png")) {
+        img.src = "/assets/moon2.png"
+    } else {
+        img.src = "/assets/sun1.png"
+    }
+    body.classList.toggle("darktheme")
+
 }
-body.classList.toggle("darktheme")
-
-}
 
 
-const getCategories = async()=> {
+const getCategories = async () => {
     try {
-            const response = await fetch("https://backend-prod.app.hiringmine.com/api/categories/all")
-            const res = await response.json() 
-            
-            if(!res.status) {
-                return
-            }
-            let {data} = res
-            console.log(data);
-            data.slice(0, 8).map((value)=> {
-                let li = document.createElement("li")
-                li.classList.add("childbox")
-                li.innerHTML=   `  <a href="https://hiringmine.com/jobsearch?category=6499f660d7d3bec5b121e02f">
+        const response = await fetch("https://backend-prod.app.hiringmine.com/api/categories/all")
+        const res = await response.json()
+
+        if (!res.status) {
+            return
+        }
+        let { data } = res
+        console.log(data);
+        data.slice(0, 8).map((value) => {
+            let li = document.createElement("li")
+            li.classList.add("childbox")
+            li.innerHTML = `  <a href="https://hiringmine.com/jobsearch?category=6499f660d7d3bec5b121e02f">
                          
                          <img src="/assets/box.svg" alt="">
-                         <h2>${value.name}</h2>
+                         <h4 >${value.name}</h4>
                          <p>${value.postCounts} Jobs</p>
                         </a>
                     `
-                document.getElementById("catergories").appendChild(li)
-            })
-            
-            
+            document.getElementById("catergories").appendChild(li)
+        })
+
+
     } catch (error) {
         console.log(error);
-        
+
     }
 }
 
-const getJobs = async()=> {
+const getJobs = async () => {
     try {
-            const response = await fetch("https://backend-prod.app.hiringmine.com/api/jobAds/all?limit=10&pageNo=1&keyWord=&category=&isPending=false")
-            const res = await response.json() 
-            
-            if(!res.status) {
-                return
-            }
-            let {data} = res
-            console.log(data);
-            data.slice(0, 6).map((value)=> {
-                let div = document.createElement("div")
-                div.classList.add("cardBox")
-                div.innerHTML=   `            
+        const response = await fetch("https://backend-prod.app.hiringmine.com/api/jobAds/all?limit=10&pageNo=1&keyWord=&category=&isPending=false")
+        const res = await response.json()
+
+        if (!res.status) {
+            return
+        }
+        let { data } = res
+        console.log(data);
+        data.slice(0, 6).map((value) => {
+            let div = document.createElement("div")
+            div.classList.add("cardBox")
+            div.innerHTML = `            
                 <a href="https://hiringmine.com/jobsearch?jid=66967f7e058dfbf2fef3f7ed">
                     <div class="card-upperpart">
                         <div class="card-portion1">
@@ -71,7 +71,7 @@ const getJobs = async()=> {
                             </div>
                             <div class="card-portion2">
                             <p>${value.designation}</p>
-                            <span>${value.payRangeStart === 0 && value.payRangeEnd === 0 ?  "No Salary Mentioned" : `${value.payRangeStart} - ${value.payRangeEnd}`}</span>
+                            <span>${value.payRangeStart === 0 && value.payRangeEnd === 0 ? "No Salary Mentioned" : `${value.payRangeStart} - ${value.payRangeEnd}`}</span>
                         </div>
 
                     </div>
@@ -89,14 +89,51 @@ const getJobs = async()=> {
 
                 </a>
             `
-                document.getElementById("jobParentBox").appendChild(div)
-            })
-            
-            
+            document.getElementById("jobParentBox").appendChild(div)
+        })
+
+
     } catch (error) {
         console.log(error);
-        
+
     }
 }
+const getHighProfile = async () => {
+    try {
+        const response = await fetch("https://backend-prod.app.hiringmine.com/api/users/home?sortBy=mostViewed")
+        const res = await response.json()
+
+        if (!res.status) {
+            return
+        }
+        let { data } = res
+        console.log(data);
+        data.slice(0, 6).map((value) => {
+            let div = document.createElement("div")
+            div.classList.add("highProfileBox")
+            div.innerHTML = `            
+              
+                <div class="highProfileImage">
+                    <img src="${value.profilePic ? value.profilePic : "https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg"}" alt="">
+                </div>
+                <div>
+                    <p class="small" style="color: #4d3bdb">${value.firstName} ${value.lastName}</p>
+                    <p class="small" style="color: grey">${value.jobTitle.slice(0, 16)}...</p>
+                    <p class="small" href="#" style="color: grey !important">View Profile &rightarrow;</p>
+                </div>
+
+                <div class="highProfileImage" style="height: 30px;"><img src="./assets/icon.png" alt=""></div>
+           
+            `
+            document.getElementById("highProfileContainer").appendChild(div)
+        })
+
+
+    } catch (error) {
+        console.log(error);
+
+    }
+}
+getHighProfile()
 getJobs()
-// getCategories()
+getCategories()
